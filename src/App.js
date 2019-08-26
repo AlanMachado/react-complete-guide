dfe1734853e23d95c09b5558815ef1ee4664a26a
AlanMachado <alanlm.1901@gmail.com>
2019-08-26 15:05:50 -0300

    State and events usage

--- src/App.js
+++ src/App.js
@@ -5,38 +5,59 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { name: 'Alan', age: 30 },
+      { name: 'William', age: 26 },
+      { name: 'Uana', age: 22 }
     ],
     otherState: 'some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
+        { name: newName, age: 30 },
+        { name: 'William', age: 26 },
+        { name: 'Uana', age: 22 }
       ]
     });
   };
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Alan', age: 30 },
+        { name: event.target.value, age: 26 },
+        { name: 'Uana', age: 22 }
+      ]
+    });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button style={style}  onClick={() => this.switchNameHandler('ALAN')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          changeName={this.c}
         />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'alan')}
+          changeName={this.nameChangeHandler}
         >
           My Hobbies: Racing
         </Person>
