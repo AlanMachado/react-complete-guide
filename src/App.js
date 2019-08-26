8562e2f07b8568bca5be9d8cfcb4b7b9a81dc348
AlanMachado <alanlm.1901@gmail.com>
2019-08-26 12:59:35 -0300

    Functional component and props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
+      <Person name="Alan" age="30"/>
+      <Person name="William" age="26">My Hobbies: UBG</Person>
+      <Person name="Uana" age="22"/>
     </div>
   );
 }
