320bb0641fc7bb9ab5c5d165be912aaac7b58620
AlanMachado <alanlm.1901@gmail.com>
2019-08-26 14:08:11 -0300

    Example using React Hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+const App = props => {
+
+  const [personsSate, setPersonsState] = useState({persons: [{name: 'Alan', age:30}, {name: 'William', age: 26}, {name: 'Uana', age: 22}]});
+
+  /* state = {
+    persons: [{name: 'Alan', age: 30}, {name: 'William', age: 26}, {name: 'Uana', age: 22}]
+  } */
+
+  const switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DONT DO THIS this.state.persons[0].name="";
+    setPersonsState({persons: [{name: 'ALAN', age: 30}, {name: 'William', age: 26}, {name: 'Uana', age: 22}] })
+  }
+  
+  
   return (
     <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <Person name="Alan" age="30"/>
-      <Person name="William" age="26">My Hobbies: UBG</Person>
-      <Person name="Uana" age="22"/>
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsSate.persons[0].name} age={personsSate.persons[0].age}/>
+        <Person name={personsSate.persons[1].name} age={personsSate.persons[1].age}>My Hobbies: UBG</Person>
+        <Person name={personsSate.persons[2].name} age={personsSate.persons[2].age}/>
     </div>
   );
+  
 }
 
 export default App;
