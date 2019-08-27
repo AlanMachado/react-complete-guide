c019fe11439bf27269655ad13ae24d614de1504f
AlanMachado <alanlm.1901@gmail.com>
2019-08-27 12:54:27 -0300

    Change the css to be used as Modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person';
-import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -40,20 +39,11 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'blue',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightblue',
-      }
-    };
-
     let persons = null;
+    let btnClass = '';
+
     if (this.state.showPersons) {
+      btnClass = classes.Black;
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
@@ -66,32 +56,26 @@ class App extends Component {
           })}
         </div>
       );
-
-      style.backgroundColor = 'black';
-      style[':hover'] = {
-        backgroundColor: 'gray'
-      }
+          
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold;')
+      assignedClasses.push(classes.bold)
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button style={style}  onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className={classes.App}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
